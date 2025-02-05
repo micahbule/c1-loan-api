372d1cc7dd1c7eedba33b194946f3b153bc527af
Micah Bule <hi@meeco.dev>
2025-02-05 17:36:15 +0800

    feat: initial implementation for loan entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoansModule } from './loans/loans.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Loan as LoanEntity } from './loans/entities/loan.entity';
 
 @Module({
-  imports: [LoansModule],
+  imports: [
+    LoansModule,
+    MikroOrmModule.forRoot({
+      entities: [LoanEntity],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/loans/entities/loan.entity.ts
+++ src/loans/entities/loan.entity.ts
@@ -1,1 +1,28 @@
-export class Loan {}
+import { Entity, Enum, PrimaryKey, Property } from '@mikro-orm/core';
+
+export enum LoanStatus {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  REJECTED = 'REJECTED',
+}
+
+@Entity()
+export class Loan {
+  @PrimaryKey({ type: 'uuid', defaultRaw: 'gen_random_uuid()' })
+  id: string;
+
+  @Property()
+  applicantName: string;
+
+  @Property()
+  requestedAmount: number;
+
+  @Enum(() => LoanStatus)
+  status: LoanStatus;
+
+  @Property()
+  createdAt: Date;
+
+  @Property()
+  updatedAt: Date;
+}
