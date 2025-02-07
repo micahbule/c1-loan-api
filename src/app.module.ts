54b3114e88a3f8edcd7568115109cddf8b2642a8
Micah Bule <hi@meeco.dev>
2025-02-07 15:09:03 +0800

    feat: remove dockerfile for now; add config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,24 @@ import { AppService } from './app.service';
 import { LoansModule } from './loans/loans.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import mikroOrmConfig from './mikro-orm.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [LoansModule, MikroOrmModule.forRoot(mikroOrmConfig)],
+  imports: [
+    LoansModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MikroOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          ...mikroOrmConfig,
+          host: configService.get('DB_HOST') || 'localhost',
+          port: configService.get('DB_PORT') || 5432,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Running migrations..."
-pnpm run migrate:up & PID=$!
-wait $PID
-
-echo "Starting production server..."
-node dist/main & PID=$!
-wait $PID
